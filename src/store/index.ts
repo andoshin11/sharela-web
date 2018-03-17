e7c0d07adee035b693f06b74aa15160239871624
andoshin11 <andoshin11@mercari.com>
2018-03-18 00:19:43 +0900

    Add store directory

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,11 @@
-import Vue from "vue";
 import Vuex from "vuex";
+import Vue from "vue";
 
+// Register Vuex
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    hoge: 'hoge'
+  }
 });
