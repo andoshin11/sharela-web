4a87b285c54bbedefb3b9199f2399ba45b69c59a
andoshin11 <andoshin11@mercari.com>
2018-03-18 02:08:41 +0900

    Add sign in and sign out button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,47 @@
   <div class="home">
     <img src="../assets/logo.png">
     <HelloWorld msg="Welcome to my Firebase App"/>
-    <img src="../assets/login_line.png" class="login">
+    <h2>{{ user ? user.displayName : '' }}</h2>
+    <img src="../assets/login_line.png" class="login" @click="login">
+    <button @click="logout">logout</button>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Vue from "vue"
+import { mapState } from 'vuex'
+import * as firebase from "firebase"
+
+import HelloWorld from "@/components/HelloWorld.vue"
+
+const provider = new firebase.auth.GoogleAuthProvider()
 
 export default Vue.extend({
   name: "home",
   components: {
     HelloWorld
+  },
+  computed: {
+    ...mapState('user', ['user'])
+  },
+  methods: {
+    login (): void {
+      console.log('logging in')
+      firebase.auth().signInWithRedirect(provider)
+    },
+    logout (): void {
+      firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+        console.log('sign out successful')
+      }).catch(function(error) {
+        // An error happened.
+        console.log('sign out failed')
+      });
+    }
+  },
+  mounted () {
+    console.log('vuex test')
+    console.log(this.$store)
   }
 });
 </script>
