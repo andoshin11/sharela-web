acd0458fd7d3b37cd277069070f993e99e178bcb
andoshin11 <andoshin11@mercari.com>
2018-03-17 22:47:40 +0900

    Initialize firebase app

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,20 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
+import * as firebase from "firebase"
 
 Vue.config.productionTip = false;
+
+// initialize firebase
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID
+}
+firebase.initializeApp(firebaseConfig)
 
 new Vue({
   router,
