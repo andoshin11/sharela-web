219b4693ec68721d7c2f617441c127a6b035a67a
andoshin11 <andoshin11@mercari.com>
2018-03-18 18:29:42 +0900

    Remove Nav

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,6 @@
   <el-container id="app">
     <el-header>
       <the-header/>
-      <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
-      </div>
     </el-header>
     <el-main>
       <router-view/>
@@ -83,15 +79,5 @@ export default Vue.extend({
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
