76919096b42f17a9d33ca766c786776806c123ba
andoshin11 <andoshin11@mercari.com>
2018-03-18 19:00:44 +0900

    Show Member list

--- src/views/Members.vue
+++ src/views/Members.vue
@@ -1,12 +1,44 @@
 <template>
   <div class="members">
-    members
+    <el-table
+      :data="users"
+      style="width: 100%">
+      <el-table-column
+        prop="displayName"
+        label="Name"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="email"
+        label="Email"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="uid"
+        label="uid">
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { mapState, mapActions } from 'vuex'
+
+// vuex
+import { GET_USERS } from '@/store/modules/user/actionTypes'
+
 export default Vue.extend({
-  
+  computed: {
+    ...mapState('user', {
+      users: state => state.list
+    })
+  },
+  methods: {
+    ...mapActions('user', [GET_USERS])
+  },
+  mounted () {
+    this.GET_USERS()
+  }
 })
 </script>
